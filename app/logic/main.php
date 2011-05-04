4f0759118017f322e8dc0cdda5202f00a4218d30
Lloyd Moore <manchesterboy@gmail.com>
2011-05-04 13:18:15 +0200

    update

--- app/logic/main.php
+++ app/logic/main.php
@@ -13,11 +13,14 @@ class main extends controller {
 		));
 	}
 
+	/**
+	 * This is called when the user clicks the link in the email
+	 */
 	public function playwin($request){
-		$ident           = new ident();
-		$alias           = $ident->getAliasForUser();
-		$subscription    = new subscription();
-		$create_response = $subscription->createSubscriptionSession(array(
+		$ident = new ident();
+		$alias = $ident->getAliasForUser();
+		$sub   = new subscription();
+		$out   = $sub->createSubscriptionSession(array(
 			'username'          => config::read('subscription_username', 'app'),
 			'password'          => config::read('subscription_password', 'app'),
 			'returnURL'         => 'http://juganar.com/main/ok',
@@ -29,38 +32,81 @@ class main extends controller {
 			'tariffClass'       => 'EUR290',
 			'tariffClassId'     => 'EUR290'
 		));
-		if ($create_response->responseMessage == 'Success') {
-			$this->r->set('session:'.session_id(), $create_response->sessionId);
+		if ($out->responseMessage == 'Success') {
+			$this->r->set('session:'.session_id(), $out->sessionId);
 			header("Location: ".$create_response->redirectURL);
 			exit();
 		} 
 	}
 
 	/**
-	 * Finalize subscription
+	 * This is called after the user is redirected to IPX 
+	 * and then IPX redirects them to us with the url 
+	 * provided by ourselves, which in this case is /main/ok.
 	 */
 	public function ok($request){
-		$this->checkStatus();
-		$this->finalizeSession();
-		$this->createSubscription();
+		$details = config::read('defaults', 'ipx');
+		$user    = $details['username2'];
+		$pwd     = $details['password2'];
+		$status  = $this->checkStatus($user, $pwd);
+		if ($status->responseMessage == 'Activated') {
+			$out = $this->finalizeSubscriptionSession($user, $pwd);
+			if ($out->responseMessage == 'Success') {
+				$this->r->saveReverse($out->consumerId, $out->subscriptionId, 'subscription'); 
+				$out = $this->chargeUser($user, $pwd);
+			}
+			return $out;
+		} else {
+			return stdClass();
+		}
 	}
 
-	public function terminateSubscription($request){
-		$subscription    = new subscription();
-		$phone           = helpers::cleanPhoneNumber($_GET['consumerId');
-		$subscription_id = $_GET['subscription_id'];
-		$response        = $subscription->terminateSubscription(array(
-			'consumerId'     => $phone,
+	/**
+	 * Charge the user
+	 */
+	private function chargeUser($user, $pwd){
+		$sub = new subscription();
+		$out = $sub->authorizePayment(array(
+			'username'       => $user,
+			'password'       => $pwd,
+			'consumerId'     => $this->r->get('session:'.session_id()),
 			'subscriptionId' => $subscription_id
+		));
+		if ($out->responseMessage == 'Success') {
+			$out = $sub->capturePayment(array(
+				'username'  => $user,
+				'password'  => $pwd,
+				'sessionId' => $out->sessionId
+			));
+			echo 'charged';
+			return $out;
+		} else {
+			return new stdClass();
+		}
+	}
+
+	/**
+	 * This request is initiated by the user in their terminal, but the actual 
+	 * request directly comes from the IPX servers as a GET request.
+	 */
+	public function terminateSubscription($request){
+		$sub      = new subscription();
+		$con_id   = helpers::cleanPhoneNumber($_GET['consumerId']);
+		$sub_id   = $_GET['subscriptionId'];
+		$response = $sub->terminateSubscription(array(
+			'consumerId'     => $con_id,
+			'subscriptionId' => $sub_id
 		));
 	}
 
-	private function sendInitialSms($phone) {
+	public function sendInitialSms($phone) {
 		$msg      = config::read('free', 'messages');
 		$sms      = new sms();
 		$out      = $sms->sendSms($phone, $msg, 'EUR0ES');
 		$choice   = $out->responseMessage == 'Success';
+
 		$this->r->recordEvent('send_sms', $phone, $out);
+
 		$headings = array('Rejected', 'Accepted');
 		$messages = array('no'      , ''        );
 		$this->template('main/activated', array(
@@ -71,29 +117,31 @@ class main extends controller {
 		));
 	}
 
-	private function checkStatus(){
-		$details    = config::read('defaults', 'ipx');
-		$user       = $details['username2'];
-		$pwd        = $details['password2'];
+	private function checkStatus($user, $pwd){
 		$session_id = $this->r->get('session:'.session_id());
-		$ident = new ident();
-		$out = $ident->checkStatus(array(
+		$ident      = new ident();
+		$out        = $ident->checkStatus(array(
 			'username'  => $user,
 			'password'  => $pwd,
 			'sessionId' => $session_id
 		));
+		return $out;
+	}
 
-		if ($out->statusMessage ==  'Authenticated') {
+	private function finalizeSubscriptionSession($user, $pwd, $status){
+		if (!($status && $status->responseMessage == 'Activated')) {
+			$session_id = $this->r->get('session:'.session_id());
+			$ident = new ident();
 			$out = $ident->finalizeSession(array(
 				'username'  => $user,
 				'password'  => $pwd,
 				'sessionId' => $session_id
 			));
-		}
-
-		if ($out->responseMessage == 'Success'){
-			$this->r->recordEvent('finalize_subscription', $session_id, $out);
+			return $out;
+		} else {
+			// Return the same type whenever possible, even 
+			// in crappy type-poo php
+			return new stdClass();
 		}
 	}
-
 }
