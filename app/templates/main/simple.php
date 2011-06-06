9ca02875c32eb97c1aa8fa593fc3e3f49072c5c0
Lloyd Moore <manchesterboy@gmail.com>
2011-06-06 12:04:42 +0200

    simplified page

---
+++ app/templates/main/simple.php
@@ -0,0 +1,20 @@
+    <div style="width:240px; height:320px; display:block; background:url(images/manolito_bg.jpg) no-repeat; margin:0 auto; position:relative;">
+        <a 
+        href="http://juganar.com/main/playwin" 
+        style="
+            width:124px; 
+            height:72px; 
+            display:block; 
+            background:url(images/btn.jpg); 
+            text-align:center; 
+            color:#000; 
+            text-decoration:none; 
+            padding-top:10px; 
+            position:absolute; 
+            z-index:2; 
+            top:80px; 
+            left:105px;">
+            Haz click aquí y llévate un fantástico <b>iPad GRATIS</b>
+        </a>
+    </div>
+
