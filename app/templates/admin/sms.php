9c2308d56ed0d21ca85f71c4e5946f4a47d3db9b
Lloyd Moore <manchesterboy@gmail.com>
2011-07-29 09:32:50 +0200

    allow opening and closing of data

--- app/templates/admin/sms.php
+++ app/templates/admin/sms.php
@@ -1,6 +1,21 @@
 <pre>
 <? foreach ($sms_responses as $sms): ?>
 
-    <? print_r($sms) ?>
+	<? $id = $sms['messageId'] ?>
+	<a href='#' class='openbox' box='<?=$id?>'>
+		<?=$id?>
+	</a>
+	<div id="<?=$id?>" style='visibility:hidden'>
+		<? print_r($sms) ?>
+	</div>
 
 <? endforeach ?>
+
+<script type='text/javascript'>
+	function click(x){
+		document.getByElementId(x.getAttribute('box')).style.visibility=''; 
+		return false;
+	};
+	var elms = document.getElementsByClassName('openbox');
+    for(var x in elms) {x.onclick = function(){click(x);};};
+</script>
